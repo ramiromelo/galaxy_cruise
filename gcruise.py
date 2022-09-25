5148660e026647bca77cb2fc4aa873be2e264fee
unknown <ramiromelo@gmail.com>
2022-09-24 20:06:10 -0700

    terceiro commit

--- gcruise.py
+++ gcruise.py
@@ -1,1 +1,21 @@
-print("Hello World")
+from pynput import keyboard
+
+def on_activate_a():
+    print('A pressed')
+
+def on_activate_b():
+    print('B pressed')
+
+def on_activate_c():
+    print('C pressed')
+
+def quit():
+    print('QUIT')
+    h.stop()
+
+with keyboard.GlobalHotKeys({
+        'a': on_activate_a,
+        'b': on_activate_b,
+        'c': on_activate_c,
+        '<ctrl>+c': quit}) as h:
+    h.join()
